f067865328e47f6eb2219796e68efea75eed4f54
gsiandroid1 <gsiandroid1@benchmarkitsolutions.com>
2016-03-09 14:28:21 +0530

    added build variants and screenshots and apks

--- app/src/main/java/com/test/assignment2/MainActivity.java
+++ app/src/main/java/com/test/assignment2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +16,7 @@ import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.test.assignment2.Constants;
 import com.test.assignment2.loader.TransportInfoLoader;
 import com.test.assignment2.pojo.TransportInfo;
 import com.test.assignment2.utils.Utility;
@@ -36,6 +38,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Constants.type== Constants.Type.DEBUG){
+            Log.i("TAG", "DEBUG VERSION");
+
+        }else {
+            Log.i("TAG", "RELEASE VERSION");
+        }
+
         initViews();
         loadData();
     }
